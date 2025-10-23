81cb30ad80ca13d75fedbe69da4c49e4282553bc
MohideenER <149752972+MohideenER@users.noreply.github.com>
2025-10-23 13:33:00 +0530

    Update BaseTest.java
    Added import in base class for chromeoption and UUID

--- src/test/java/com/orangehrm/BaseTest.java
+++ src/test/java/com/orangehrm/BaseTest.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.UUID;
+
 
 public class BaseTest {
     protected WebDriver driver;
